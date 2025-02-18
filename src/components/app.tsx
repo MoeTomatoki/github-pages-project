1df5a389b4486a369efaf9e0a2ecd01c46997823
MoeTomatoki <djprofi7@gmail.com>
2025-02-18 21:40:22 +0300

    рабочая карусель с использованием framer-motion

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,48 +1,50 @@
 import clsx from "clsx";
-import { useEffect, useRef, useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { motion, useMotionValue } from "framer-motion";
 
-const imgPath = [
+const imgsPath = [
     "/public/sprint-3-images/cover-trains.jpg",
     "public/sprint-3-images/place-kosa.jpg",
     "public/sprint-3-images/place-kolsky.jpg",
     "public/sprint-3-images/place-altai.jpg",
     "public/sprint-3-images/place-karelia.jpg",
-]
+    "public/sprint-3-images/place-winter-baikal.jpg",
+    "public/sprint-3-images/photo-grid-arisa.jpg",
+];
 
 export default function App() {
 
     return (
-        <main className="p-14 bg-primary-bg">
+        <main className="relative min-h-screen px-[5vw] py-8 overflow-hidden bg-primary-bg">
             <MainGallery />
         </main>
     )
 }
 
-export function MainGallery() {
-    const [firstRender, setFirstRender] = useState<boolean>(true);
-    const [imgSelector, setImgSelector] = useState<number>(0);
-    const [bgImage, setBgImage] = useState<string>(imgPath[imgSelector]);
+const DRAG_RANGE = 50;
+const DELAY_INTERVAL = 3000;
 
-    const [transitioning, setTransitioning] = useState<boolean>(false);
+export function MainGallery() {
+    const [carouselIndex, setCarouselIndex] = useState<number>(0);
+    const [, setIsDragging] = useState<boolean>(false);
+
+    const firstRender = useRef<boolean | null>(true);
     const intervalRef = useRef<number | null>(null);
     const clearIntervalRef = useRef<number | null>(null);
 
-    setTimeout(() => setFirstRender(false), 3000);
-    
-    const intervalHandler = (imgPath: string[], prev: number) => {
-        if (prev === imgPath.length - 1) return 0;
+    const dragX = useMotionValue(0);
+
+    setTimeout(() => firstRender.current = null, DELAY_INTERVAL);
+
+    const intervalHandler = (prev: number) => {
+        if (prev === imgsPath.length - 1) return 0;
         return prev + 1;
     }
 
     if (intervalRef.current === null) {
         intervalRef.current = setInterval(() => {
-            setTransitioning(true);
-            setImgSelector(prev => {
-                const newSelector = intervalHandler(imgPath, prev);
-                setBgImage(imgPath[newSelector]);
-                return newSelector;
-            });
-        }, 3000);
+            setCarouselIndex(prev => intervalHandler(prev));
+        }, DELAY_INTERVAL);
     }
 
     if (clearIntervalRef.current === null) {
@@ -51,42 +53,102 @@ export function MainGallery() {
                 clearInterval(intervalRef.current);
                 intervalRef.current = null;
             }
-        }, 3000);
+        }, DELAY_INTERVAL);
     }
 
-    const onTransitionEnd = () => {
-        setTransitioning(false);
-    };
+    const onDragStart = () => {
+        setIsDragging(true);
+    }
 
+    const onDragEnd = () => {
+        setIsDragging(false);
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setImgSelector(prev => {
-    //             const newSelector = intervalHandler(imgPath, prev);
-    //             setBgImage(imgPath[newSelector]);
-    //             return newSelector
-    //         });
-    //     }, 3000);
-    //     return () => clearInterval(interval);
-    // }, []);
+        const x = dragX.get();
+
+        if (x <= -DRAG_RANGE && carouselIndex < imgsPath.length - 1) {
+            setCarouselIndex(prev => prev + 1);
+        }
+        if (x > DRAG_RANGE && carouselIndex > 0) {
+            setCarouselIndex(prev => prev - 1);
+        }
+    }
 
     return (
-        <section className="relative">
-            <div
-                className={clsx(`absolute inset-0 bg-cover bg-center
-                    transition-opacity duration-1500 ease-[cubic-bezier(0.4,0.4,0.8,0.8)]`, 
-                    `${firstRender ? "opacity-65" : !transitioning ? "opacity-0" : "opacity-65"}`)}
-                style={{ backgroundImage: `url(${bgImage})` }}
-                onTransitionEnd={onTransitionEnd}
-            />
-            <div className="relative py-90 flex flex-col items-center justify-center text-center text-white text-5xl">
-                <h1 className="font-bold max-w-[24rem]">
-                    До Байкала «на собаках»
-                </h1>
-                <h4 className="font-thin text-base max-w-2xs break-words">
-                    По мотивам учебной темы о Транссибе — путешествие от столицы до Байкала на электричках.
-                </h4>
-            </div>
-        </section>
+        <>
+            <motion.div
+                drag="x"
+                dragConstraints={{
+                    left: 0,
+                    right: 0,
+                }}
+                animate={{
+                    translateX: `-${carouselIndex * 55.5}%`,
+                }}
+                onDragStart={onDragStart}
+                onDragEnd={onDragEnd}
+                style={{
+                    x: dragX,
+                }}
+                className="flex items-center cursor-grab active:cursor-grabbing"
+            >
+                <CarouselImage carouselIndex={carouselIndex} />
+            </motion.div>
+
+            <CarouselDots carouselIndex={carouselIndex} setCarouselIndex={setCarouselIndex} />
+        </>
     );
+}
+
+export function CarouselImage({ carouselIndex }: { carouselIndex: number }) {
+    return (
+        <>
+            {imgsPath.map((path, index) => <motion.div
+                    key={index}
+                    style={{
+                        backgroundImage: `url(${path})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                    animate={{
+                        scale: carouselIndex === index ? 0.95 : 0.85,
+                    }}
+                    transition={{
+                        type: "spring",
+                        mass: 3,
+                        stiffness: 400,
+                        damping: 50,
+                    }}
+                    className="aspect-video shrink-0 w-[50vw] rounded-xl bg-cyan-50 object-cover"
+                />
+            )}
+        </>
+    )
+}
+
+type DotsProps = {
+    carouselIndex: number;
+    setCarouselIndex: Dispatch<SetStateAction<number>>;
+}
+
+const dotRoundFunc = function (index: number) {
+    if (index === imgsPath.length / 2 || index === Math.round(imgsPath.length / 2) - 1) return "rounded-b-xl rounded-t-md"
+    else if (index <= imgsPath.length / 2) return "rounded-bl-xl rounded-tr-xl"
+    else return " rounded-br-xl rounded-tl-xl"
+}
+
+export function CarouselDots({ carouselIndex, setCarouselIndex }: DotsProps) {
+    return (
+        <div className="flex w-full justify-center gap-2 mt-4">
+            {imgsPath.map((_, index) => {
+                return <button
+                    key={index}
+                    onClick={() => setCarouselIndex(index)}
+                    className={clsx("h-5 w-5 bg-cyan-50 transition-colors",
+                        `${index === carouselIndex ? "bg-white" : "bg-neutral-500"}`,
+                        `${dotRoundFunc(index)}`
+                    )}
+                />
+            })}
+        </div>
+    )
 }
